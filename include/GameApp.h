65ccc946c16bc9a1a93b21e1fa6281aee0b3b560
JeffreyMJohnson <jefjohms@gmail.com>
2015-08-31 22:31:38 -0700

    Refactored into app
    Game loop is now a base class, and implemented.

---
+++ include/GameApp.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <gl_core_4_4.h>
+#include <GLFW\glfw3.h>
+#include <aie\Gizmos.h>
+#include <glm\glm.hpp>
+#include <glm\ext.hpp>
+#include <glm\gtx\transform.hpp>
+#include "Timer.h"
+
+
+class GameApp
+{
+public:
+	virtual bool StartUp() = 0;
+	virtual void ShutDown() = 0;
+	virtual bool Update() = 0;
+	virtual void Draw() = 0;
+
+	GLFWwindow* mWindow = nullptr;
+	Timer timer = Timer();
+
+};
